1726367c5502cf8ae18125725b662600b3ef3849
Scott Nagy <scottanagy@gmail.com>
2017-10-19 09:57:21 -0700

    Revert "Fix sRGB cutoff value"
    This reverts commit c44d9cf4da34def556ec845ead8efd4399fe0bf5.

--- shaders/pbr-frag.glsl
+++ shaders/pbr-frag.glsl
@@ -94,7 +94,7 @@ vec4 SRGBtoLINEAR(vec4 srgbIn)
     #ifdef SRGB_FAST_APPROXIMATION
     vec3 linOut = pow(srgbIn.xyz,vec3(2.2));
     #else //SRGB_FAST_APPROXIMATION
-    vec3 bLess = step(vec3(0.04045),srgbIn.xyz);
+    vec3 bLess = step(vec3(0.03928),srgbIn.xyz);
     vec3 linOut = mix( srgbIn.xyz/vec3(12.92), pow((srgbIn.xyz+vec3(0.055))/vec3(1.055),vec3(2.4)), bLess );
     #endif //SRGB_FAST_APPROXIMATION
     return vec4(linOut,srgbIn.w);;
