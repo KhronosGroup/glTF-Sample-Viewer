7d1f1ec5b933f9aa900a043453f52fd91ff4191c
Max Limper <max.limper@igd.fraunhofer.de>
2018-02-20 16:59:01 +0100

    Shader comment on intensity vs reflectance

--- shaders/pbr-frag.glsl
+++ shaders/pbr-frag.glsl
@@ -281,6 +281,7 @@ void main()
     // Calculation of analytical lighting contribution
     vec3 diffuseContrib = (1.0 - F) * diffuse(pbrInputs);
     vec3 specContrib = F * G * D / (4.0 * NdotL * NdotV);
+    // Obtain final intensity as reflectance (BRDF) scaled by the energy of the light (cosine law)
     vec3 color = NdotL * u_LightColor * (diffuseContrib + specContrib);
 
     // Calculate lighting contribution from image based lighting source (IBL)
