9d893a1b07ce2674b2b9d86a651ebbc7cc8b0165
Moritz Becher <becher@ux3d.io>
2018-11-16 13:37:40 +0100

    fix headless mode

--- electron/offscreen.js
+++ electron/offscreen.js
@@ -16,6 +16,8 @@ function createWindow () {
       }
     });
 
+    //mainWindow.webContents.openDevTools();
+
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, "../headless.html"),
         protocol: 'file',
