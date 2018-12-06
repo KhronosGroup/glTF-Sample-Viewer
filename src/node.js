1905b4381bb7c490322ef68db8071af2614f8fa4
Norbert Nopper <nopper@ux3d.io>
2018-12-06 16:20:22 +0100

    Reverted matrix optimization.

--- src/node.js
+++ src/node.js
@@ -14,6 +14,7 @@ class gltfNode
         this.translation = translation;
         this.rotation = rotation;
         this.scale = scale;
+        this.matrix = undefined;
         this.children = children;
         this.camera = undefined;
         this.name = name;
@@ -67,12 +68,12 @@ class gltfNode
 
     applyMatrix(matrixData)
     {
-        this.transform = jsToGl(matrixData);
-
-        mat4.getScaling(this.scale, this.transform);
-        mat4.getRotation(this.rotation, this.transform);
-        mat4.getTranslation(this.translation, this.transform);
-
+        this.matrix = jsToGl(matrixData);
+        
+        mat4.getScaling(this.scale, this.matrix);
+        mat4.getRotation(this.rotation, this.matrix);
+        mat4.getTranslation(this.translation, this.matrix);
+        
         this.changed = true;
     }
 
@@ -104,8 +105,15 @@ class gltfNode
     {
         if(this.transform === undefined || this.changed)
         {
-            this.transform = mat4.create();
-            mat4.fromRotationTranslationScale(this.transform, this.rotation, this.translation, this.scale);
+            if (this.matrix !== undefined)
+            {
+                this.transform = this.matrix;
+            }
+            else
+            {
+                this.transform = mat4.create();
+                mat4.fromRotationTranslationScale(this.transform, this.rotation, this.translation, this.scale);
+            }
             this.changed = false;
         }
 
