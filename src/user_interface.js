4e15b01a9571c7d0f711c2fe3f5c8899ab3d34da
Benjamin Schmithüsen <schmithuesen@ux3d.io>
2019-01-07 12:02:28 +0100

    IBL can no longer be activated if texture lod extension is unavailable

--- src/user_interface.js
+++ src/user_interface.js
@@ -98,7 +98,16 @@ class gltfUserInterface
     {
         const self = this;
         const lightingFolder = this.gui.addFolder("Lighting");
-        lightingFolder.add(this.renderingParameters, "useIBL").name("Image-Based Lighting");
+        if (this.renderingParameters.useShaderLoD)
+        {
+            lightingFolder.add(this.renderingParameters, "useIBL").name("Image-Based Lighting");
+        }
+        else
+        {
+            const message = "not available";
+            const messageObject = { message: message };
+            lightingFolder.add(messageObject, "message").name("Image-Based Lighting").onChange(() => messageObject.message = message);
+        }
         lightingFolder.add(this.renderingParameters, "usePunctual").name("Punctual Lighting");
         lightingFolder.add(this.renderingParameters, "environmentName", Object.keys(Environments)).name("Environment")
             .onChange(() => self.onModelSelected(self.selectedModel));
