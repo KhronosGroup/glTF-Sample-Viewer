5025c7bc1edd26222cee60e1bdf6d3fbc5030eee
Benjamin Schmithüsen <schmithuesen@ux3d.io>
2019-11-25 14:54:18 +0100

    refactor

--- libs/ktx2image.js
+++ libs/ktx2image.js
@@ -1,36 +1,42 @@
+const VERSION = [ 0xAB, 0x4B, 0x54, 0x58, 0x20, 0x32, 0x30, 0xBB, 0x0D, 0x0A, 0x1A, 0x0A ];
+const VERSION_OFFSET = 0;
+const VERSION_LENGTH = VERSION.length;
+const HEADER_OFFSET = VERSION_OFFSET + VERSION_LENGTH;
+const HEADER_LENGTH = 9 * 4;
+
 class Ktx2Image
 {
     initialize(arrayBuffer)
     {
-        if (! this.checkVersion(arrayBuffer))
+        const version = new DataView(arrayBuffer, VERSION_OFFSET, VERSION_LENGTH);
+        if (! this.checkVersion(version))
         {
             console.error("Invalid Ktx2 version identifier");
             return;
         }
+
+        const header = new DataView(arrayBuffer, HEADER_OFFSET, HEADER_LENGTH);
+        this.parseHeader(header);
     }
 
-    checkVersion(arrayBuffer)
+    checkVersion(version)
     {
-        const expectedVersion = [ 0xAB, 0x4B, 0x54, 0x58, 0x20, 0x32, 0x30, 0xBB, 0x0D, 0x0A, 0x1A, 0x0A ];
-        const versionOffset = 0;
-        const versionLength = expectedVersion.length;
+        const VERSION_OFFSET = 0;
 
-        if (arrayBuffer.length < versionLength)
+        for (let i = 0; i < VERSION_LENGTH; i++)
         {
-            return false;
-        }
-
-        const version = new Uint8Array(arrayBuffer, versionOffset, versionLength);
-
-        for (let i = 0; i < versionLength; i++)
-        {
-            if (version[i] != expectedVersion[i])
+            if (version.getUint8(i) != VERSION[i])
             {
                 return false;
             }
         }
 
         return true;
+    }
+
+    parseHeader(header)
+    {
+
     }
 }
 
