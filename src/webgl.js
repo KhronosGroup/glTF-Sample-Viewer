a3874d2f196c1c7eddfa9368af80be7f66e7744c
Benjamin Schmithüsen <schmithuesen@ux3d.io>
2019-12-13 14:35:39 +0100

    remove debug logs

--- src/webgl.js
+++ src/webgl.js
@@ -95,18 +95,11 @@ class gltfWebGl
 
                 if (image.image instanceof Ktx2Image)
                 {
-                    // TODO: remove log
-                    console.log("creating GL texture for KTX image");
-
                     for (const level of image.image.levels)
                     {
-                        console.log("creating level");
-
                         let faceType = WebGl.context.TEXTURE_CUBE_MAP_POSITIVE_X;
                         for (const face of level.faces)
                         {
-                            console.log("creating face with type " + faceType);
-
                             WebGl.context.texImage2D(faceType, level.miplevel, WebGl.context.RGBA16F, level.width, level.height, 0, WebGl.context.RGBA, WebGl.context.HALF_FLOAT, face.data);
 
                             faceType++;
