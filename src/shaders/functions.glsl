eb16e5abbbd5004e2a02b30bbeee67703889367b
Jim Eckerlein <eckerlein@ux3d.io>
2020-03-23 14:07:11 +0100

    fix handling of total internal reflection

--- src/shaders/functions.glsl
+++ src/shaders/functions.glsl
@@ -129,10 +129,11 @@ vec3 refractionThin(vec3 v, vec3 n, float ior_1, float ior_2)
 //     return refract(r, n1,  ior_2 / ior_1);
 // }
 
-vec3 refraction(vec3 l, vec3 n, float n1, float n2) {
+vec3 refraction(vec3 l, vec3 n, float n1, float n2, out bool internal_reflection) {
     float c = dot(-n, l);
     float r = n1 / n2;
     float D = 1.0 - r * r * (1.0 - c * c);
+    internal_reflection = D < 0.0;
     return r * l + (r * c - sqrt(D)) * n;
 }
 
@@ -141,13 +142,22 @@ vec3 refraction(vec3 l, vec3 n, float n1, float n2) {
 // Models the exiting refraction that would be caused by this sphere.
 vec3 refractionSolidSphere(vec3 v, vec3 N, float ior_1, float ior_2, float thickness, out float dist)
 {
+    bool internal_reflection;
+
     vec3 r = normalize(-v);
-    vec3 r_ = normalize(refraction(normalize(r), normalize(N), ior_1, ior_2));
+    vec3 r_ = normalize(refraction(normalize(r), normalize(N), ior_1, ior_2, internal_reflection));
+    if (internal_reflection) {
+        return reflect(-v, N);
+    }
+
     dist = thickness * dot(-N, r_);
 
     vec3 a = thickness / 2.0 * -N;
     vec3 b = normalize(dist * r_ - a);
 
-    vec3 q = -refraction(-r_, b, ior_2, ior_1);
+    vec3 q = -refraction(-r_, b, ior_2, ior_1, internal_reflection);
+    if (internal_reflection) {
+        return reflect(-v, N);
+    }
     return q;
 }
