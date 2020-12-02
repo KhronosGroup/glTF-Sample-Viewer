f064d5b3fb28e72f58cc6db5d8c5a289ee06a621
Phillip Hohenester <hohenester@ux3d.io>
2020-12-02 16:05:41 +0100

    improved new UI

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -3,15 +3,32 @@ Vue.component('drop-down-element', {
   props: ['name', 'dropdowncontent'],
   template:'#dropDownTemplate'
 });
-
-Vue.component('radio-button-element', {
+Vue.component('radio-button-list-element', {
   props: ['name', 'radiobuttoncontent'],
   data() {
     return {
       radio: ""
     };
   },
-  template:'#radioButtonTemplate'
+  template:'#radioButtonListTemplate'
+});
+Vue.component('check-box-element', {
+  props: ['name', 'checkboxcontent'],
+  data() {
+    return {
+        checkboxGroup: []
+    };
+  },
+  template:'#checkBoxTemplate'
+});
+Vue.component('slider-element', {
+  props: ['name'],
+  data() {
+    return {
+        value: 0
+    };
+  },
+  template:'#sliderTemplate'
 });
 
 
@@ -26,8 +43,22 @@ Vue.component('tab-display', {
   template:'#displayTemplate'
 });
 Vue.component('tab-animation', {
-  props: [],
+  props: ["animations"],
   template:'#animationTemplate'
+});
+Vue.component('tab-xmp', {
+  props: [""],
+  template:'#xmpTemplate'
+});
+Vue.component('tab-advanced-controls', {
+  props: ["debugchannels", "tonemaps"],
+  data() {
+    return {
+        skinning: false,
+        morphing: false,
+    };
+  },
+  template:'#advancedControlsTemplate'
 });
 
 const app = new Vue({
@@ -44,7 +75,10 @@ const app = new Vue({
       scenes: [{title: "1"}, {title: "2"}],
       cameras: [{title: "front"}, {title: "left"}, {title: "right"}, {title: "top"}],
       materialVariants: [{title: "mat var yellow"}, {title: "mat var red"}, {title: "mat var blue"}],
-      environments: [{title: "mat var yellow"}, {title: "mat var red"}, {title: "mat var blue"}]
+      environments: [{title: "mat var yellow"}, {title: "mat var red"}, {title: "mat var blue"}],
+      animations: [{title: "cool animation"}, {title: "even cooler"}, {title: "not cool"}, {title: "Do not click!"}],
+      tonemaps: [{title: "ACES"}, {title: "Linear"}],
+      debugchannels: [{title: "Wireframe"}, {title: "Color"}, {title: "Specular"}, {title: "Metalic"}, {title: "Sheen"}],
     };
   }
 }).$mount('#app')
