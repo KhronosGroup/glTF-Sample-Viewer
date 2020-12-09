f9aaa68abeb6f5ff18593d74fe49adca0893554f
Moritz Willy Becher <becher@ux3d.io>
2020-12-09 18:22:39 +0100

    remove old sample viewer html and entry point

--- src/main.js
+++ src/main.js
@@ -5,65 +5,13 @@ import { DracoDecoder } from './draco.js';
 import { KtxDecoder } from './ktx.js';
 import { GltfView } from './GltfView/gltf_view.js';
 import { computePrimitiveCentroids } from './gltf_utils.js';
-import { loadGltfFromPath, loadGltfFromDrop, loadPrefilteredEnvironmentFromPath } from './ResourceLoader/resource_loader.js';
+import { loadGltfFromPath, loadPrefilteredEnvironmentFromPath } from './ResourceLoader/resource_loader.js';
 
-async function gltfSampleViewer(
-    canvasId,
-    index,
-    envMap = "Courtyard of the Doge's palace",
-    onRendererReady = undefined,
-    basePath = "",
-    initialModel = "BoomBox")
-{
-    const canvas = document.getElementById(canvasId);
-    if (!canvas)
-    {
-        console.warn("Failed to retrieve the WebGL canvas!");
-        return null;
-    }
-
-    WebGl.context = getWebGlContext(canvas);
-    if (!WebGl.context)
-    {
-        console.warn("Failed to get an WebGL rendering context!");
-        return null;
-    }
-
-    const input = new gltfInput(canvas);
-    input.setupGlobalInputBindings(document);
-    input.setupCanvasInputBindings(canvas);
-
-    const dracoDecoder = new DracoDecoder();
-    await dracoDecoder.ready();
-
-    const ktxDecoder = new KtxDecoder();
-    await ktxDecoder.init();
-
-    new gltfViewer(canvas, index, input, onRendererReady, basePath, initialModel, envMap, dracoDecoder, ktxDecoder);
-}
-
-function getWebGlContext(canvas)
-{
-    const parameters = { alpha: false, antialias: true };
-    const contextTypes = [ "webgl2" ];
-
-    let context;
-
-    for (const contextType of contextTypes)
-    {
-        context = canvas.getContext(contextType, parameters);
-        if (context)
-        {
-            return context;
-        }
-    }
-    return context;
-}
 
 async function main()
 {
     const canvas = document.getElementById("canvas");
-    WebGl.context = getWebGlContext(canvas);
+    WebGl.context = canvas.getContext("webgl2", { alpha: false, antialias: true });
     const view = new GltfView(canvas);
     const state = view.createState();
 
@@ -105,4 +53,4 @@ async function main()
     await view.startRendering(state);
 }
 
-export { gltfSampleViewer, main };
+export { main };
