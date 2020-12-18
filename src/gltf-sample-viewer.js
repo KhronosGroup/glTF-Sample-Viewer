365bfc9804efe3b0414a9f0dabb4a30a93deafad
Patrick Härtl <haertl@ux3d.io>
2020-12-18 16:31:54 +0100

    Export loadGltfFromDrop

--- src/gltf-sample-viewer.js
+++ src/gltf-sample-viewer.js
@@ -1,8 +1,8 @@
 import { GltfView } from './GltfView/gltf_view.js';
 import { computePrimitiveCentroids } from './gltf/gltf_utils.js';
-import { loadGltfFromPath, loadPrefilteredEnvironmentFromPath, initDracoLib, initKtxLib } from './ResourceLoader/resource_loader.js';
+import { loadGltfFromPath, loadPrefilteredEnvironmentFromPath, initDracoLib, initKtxLib, loadGltfFromDrop } from './ResourceLoader/resource_loader.js';
 
 import { getIsGltf, getIsGlb } from './gltf/utils.js';
 
 
-export {GltfView, getIsGltf, getIsGlb, computePrimitiveCentroids, loadGltfFromPath, loadPrefilteredEnvironmentFromPath, initKtxLib, initDracoLib };
+export {GltfView, getIsGltf, getIsGlb, computePrimitiveCentroids, loadGltfFromPath, loadPrefilteredEnvironmentFromPath, initKtxLib, initDracoLib, loadGltfFromDrop };
