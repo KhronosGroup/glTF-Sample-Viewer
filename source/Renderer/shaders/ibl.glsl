0120a8ebc5019c03509aac48772cba23a582d387
Jim Eckerlein <eckerlein@ux3d.io>
2021-03-11 14:32:37 +0100

    document IBL sampling with KHR_materials_volume

--- source/Renderer/shaders/ibl.glsl
+++ source/Renderer/shaders/ibl.glsl
@@ -58,42 +58,50 @@ vec3 getIBLRadianceTransmission(vec3 n, vec3 v, vec2 fragCoord, float perceptual
 vec3 getIBLVolumeRefraction(vec3 normal, vec3 viewDirectionW, float perceptualRoughness, vec3 baseColor, vec3 f0, vec3 f90,
     vec3 worldPos, mat4 modelMatrix, mat4 viewMatrix, mat4 projMatrix, float ior, float thickness, vec3 attenuationColor, float attenuationDistance)
 {
+    // Direction of refracted light.
+    vec3 refractionVector = refract(-viewDirectionW, normalize(normal), 1.0 / ior);
+
+    // Compute rotation-independant scaling of the model matrix.
     vec3 modelScale;
     modelScale.x = length(vec3(modelMatrix[0].xyz));
     modelScale.y = length(vec3(modelMatrix[1].xyz));
     modelScale.z = length(vec3(modelMatrix[2].xyz));
 
-    vec3 refractionVector = refract(-viewDirectionW, normalize(normal), 1.0 / ior);
-
-    vec3 refractedRayExitW = worldPos + normalize(refractionVector) * thickness * modelScale;
+    // Point where the refracted light is assumed to exit the geometry again.
+    // The thickness is specified in local space.
+    vec3 refractedRayExit = worldPos + normalize(refractionVector) * thickness * modelScale;
     float transmissionDistance = thickness * length(modelScale);
 
-    vec4 viewPos = viewMatrix * vec4(refractedRayExitW, 1.0);
-    vec4 ndcPos = projMatrix * viewPos;
+    vec4 viewPos = viewMatrix * vec4(refractedRayExit, 1.0);
 
+    // Project refracted vector on the framebuffer, while mapping to normalized device coordinates.
+    vec4 ndcPos = projMatrix * viewPos;
     vec2 refractionCoords = ndcPos.xy / ndcPos.z;
     refractionCoords += 1.0;
     refractionCoords /= 2.0;
 
-    // Sample GGX LUT.
-    float NdotV = clampedDot(normal, viewDirectionW);
-    vec2 brdfSamplePoint = clamp(vec2(NdotV, perceptualRoughness), vec2(0.0, 0.0), vec2(1.0, 1.0));
-    vec2 brdf = texture(u_GGXLUT, brdfSamplePoint).rg;   
-    vec3 specularColor = f0 * brdf.x + f90 * brdf.y;
-
+    // Sample framebuffer to get pixel the refracted ray hits.
     vec3 transmittedLight = getTransmissionSample(refractionCoords, perceptualRoughness);
 
     vec3 attenuatedColor;
     if (attenuationDistance == 0.0)
     {
+        // Attenuation distance is +∞ (which we indicate by zero), i.e. the transmitted color is not attenuated at all.
         attenuatedColor = transmittedLight;
     }
     else
     {
+        // Compute light attenuation using Beer's law.
         vec3 attenuationCoefficient = -log(attenuationColor) / attenuationDistance;
-        vec3 transmittance = exp(-attenuationCoefficient * transmissionDistance); // Beer's law
+        vec3 transmittance = exp(-attenuationCoefficient * transmissionDistance);
         attenuatedColor = transmittance * transmittedLight;
     }
+
+    // Sample GGX LUT to get the specular component.
+    float NdotV = clampedDot(normal, viewDirectionW);
+    vec2 brdfSamplePoint = clamp(vec2(NdotV, perceptualRoughness), vec2(0.0, 0.0), vec2(1.0, 1.0));
+    vec2 brdf = texture(u_GGXLUT, brdfSamplePoint).rg;   
+    vec3 specularColor = f0 * brdf.x + f90 * brdf.y;
 
     return (1.0 - specularColor) * attenuatedColor * baseColor;
 }
