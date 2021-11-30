2971a7825de2f3ba7f58ced46f051afec34846cd
Patrick Härtl <haertl@ux3d.io>
2021-11-30 15:16:07 +0100

    Fix it in metallic roughness as well (https://github.com/KhronosGroup/glTF/pull/2022)

--- source/Renderer/shaders/material_info.glsl
+++ source/Renderer/shaders/material_info.glsl
@@ -204,7 +204,7 @@ MaterialInfo getMetallicRoughnessInfo(MaterialInfo info)
 #endif
 
     // Achromatic f0 based on IOR.
-    info.c_diff = mix(info.baseColor.rgb * (vec3(1.0) - info.f0),  vec3(0), info.metallic);
+    info.c_diff = mix(info.baseColor.rgb,  vec3(0), info.metallic);
     info.f0 = mix(info.f0, info.baseColor.rgb, info.metallic);
     return info;
 }
