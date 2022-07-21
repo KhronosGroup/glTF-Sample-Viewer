5d250ffbb2358d01e84ff81e1ed5070e046fb7a1
Mathias Kanzler <kanzler@ux3d.io>
2022-07-21 12:40:58 +0200

    Split graphics renderer and audio renderer

--- source/Renderer/renderer.js
+++ source/Renderer/renderer.js
@@ -32,8 +32,6 @@ class gltfRenderer
         this.initialized = false;
         this.samples = 4;
 
-        this.audioContext = undefined;
-
         // create render target for non transmission materials
         this.opaqueRenderTexture = 0;
         this.opaqueFramebuffer = 0;
@@ -256,179 +254,6 @@ class gltfRenderer
         this.transmissionDrawables = drawables
             .filter(({node, primitive}) => state.gltf.materials[primitive.material].extensions !== undefined
                 && state.gltf.materials[primitive.material].extensions.KHR_materials_transmission !== undefined);
-
-        this.audioEmitterNodes = this.nodes
-        .filter(node => node.extensions !== undefined && node.extensions.KHR_audio !== undefined && node.extensions.KHR_audio.emitter !== undefined);
-
-        if(this.audioContext !== undefined)
-        {
-            this.audioContext.close()
-        }
-        
-        this.audioContext = new AudioContext();
-    }
-
-
-    updateAudioEmitter(state, emitter)
-    {
-        // emitterSourceNode -> [optional: pannerNode] -> gainNode -> globalDestination
-
-        const source = state.gltf.audioSources[emitter.source]
-
-        // At first create audio nodes if they are undefined
-
-        // controls emitter gain
-        if(emitter.gainNode === undefined)
-        {
-            emitter.gainNode = this.audioContext.createGain()
-            emitter.gainNode.connect(this.audioContext.destination)
-        }
-           
-        if(emitter.pannerNode === undefined) 
-        {                          
-            emitter.pannerNode = this.audioContext.createPanner();
-        }
-    
-        // audio source node
-        if(emitter.audioBufferSourceNode === undefined)
-        {
-            emitter.audioBufferSourceNode = this.audioContext.createBufferSource();
-            emitter.audioBufferSourceNode.buffer = source.decodedAudio;
-            if(emitter.type === "global")
-            {   
-                // skip panner node if we have a global emitter
-                emitter.audioBufferSourceNode.connect(emitter.gainNode);
-            }
-            else
-            {
-                emitter.audioBufferSourceNode.connect(emitter.pannerNode);
-                emitter.pannerNode.connect(emitter.gainNode)
-            }
-
-            if(emitter.playing === true)
-            {
-                emitter.audioBufferSourceNode.start();
-            }
-
-        }
-
-        // Update values:
-        emitter.gainNode.gain.setValueAtTime(emitter.gain, this.audioContext.currentTime);
-
-        emitter.pannerNode.distanceModel = emitter.positional.distanceModel;
-        emitter.pannerNode.refDistance = emitter.positional.refDistance;
-        emitter.pannerNode.maxDistance = emitter.positional.maxDistance;
-        emitter.pannerNode.rolloffFactor = emitter.positional.rolloffFactor;
-        // pannerNode angles are given in degrees. glTF values are given in radians
-        emitter.pannerNode.coneInnerAngle = emitter.positional.coneInnerAngle * 180.0 / Math.PI;
-        emitter.pannerNode.coneOuterAngle = emitter.positional.coneOuterAngle * 180.0 / Math.PI;
-        emitter.pannerNode.coneOuterGain = emitter.positional.coneOuterGain;
-
-        if(emitter.pannerNode.orientationX) 
-        {
-            emitter.pannerNode.orientationX.setValueAtTime(emitter.orientation[0], this.audioContext.currentTime);
-            emitter.pannerNode.orientationY.setValueAtTime(emitter.orientation[1], this.audioContext.currentTime);
-            emitter.pannerNode.orientationZ.setValueAtTime(emitter.orientation[2], this.audioContext.currentTime);
-        } 
-        else 
-        {
-            // Firefox
-            emitter.pannerNode.setOrientation(emitter.orientation[0],emitter.orientation[1],emitter.orientation[2]);
-        }
-
-        if(emitter.pannerNode.positionX) 
-        {
-            emitter.pannerNode.positionX.setValueAtTime(emitter.position[0], this.audioContext.currentTime);
-            emitter.pannerNode.positionY.setValueAtTime(emitter.position[1], this.audioContext.currentTime);
-            emitter.pannerNode.positionZ.setValueAtTime(emitter.position[2], this.audioContext.currentTime);
-        } 
-        else 
-        {   
-            // Firefox
-            emitter.pannerNode.setPosition(emitter.position[0], emitter.position[1], emitter.position[2]);
-        }
-        emitter.audioBufferSourceNode.loop = emitter.loop;
-    }
-
-    updateAudioListener()
-    {
-        var listener = this.audioContext.listener;
-
-        if(listener.forwardX) {
-            listener.forwardX.setValueAtTime(this.currentCameraLookDirection[0], this.audioContext.currentTime);
-            listener.forwardY.setValueAtTime(this.currentCameraLookDirection[1], this.audioContext.currentTime);
-            listener.forwardZ.setValueAtTime(this.currentCameraLookDirection[2], this.audioContext.currentTime);
-            listener.upX.setValueAtTime(this.currentCameraUpDirection[0], this.audioContext.currentTime);
-            listener.upY.setValueAtTime(this.currentCameraUpDirection[1], this.audioContext.currentTime);
-            listener.upZ.setValueAtTime(this.currentCameraUpDirection[2], this.audioContext.currentTime);
-        } 
-        else 
-        {
-            // Firefox requires deprecated function
-            listener.setOrientation(
-                this.currentCameraLookDirection[0],
-                this.currentCameraLookDirection[1],
-                this.currentCameraLookDirection[2],
-                this.currentCameraUpDirection[0],
-                this.currentCameraUpDirection[1],
-                this.currentCameraUpDirection[2]);
-        }
-
-        if(listener.positionX) 
-        { 
-            listener.positionX.setValueAtTime(this.currentCameraPosition[0], this.audioContext.currentTime);
-            listener.positionY.setValueAtTime(this.currentCameraPosition[1], this.audioContext.currentTime);
-            listener.positionZ.setValueAtTime(this.currentCameraPosition[2], this.audioContext.currentTime);
-        } 
-        else 
-        {   
-            // Firefox requires deprecated function
-            listener.setPosition(this.currentCameraPosition[0], this.currentCameraPosition[1], this.currentCameraPosition[2]);
-        }
-    }
-
-    handleAudio(state, scene)
-    {
-        if (this.preparedScene !== scene) 
-        {
-            this.prepareScene(state, scene)
-            this.preparedScene = scene
-        }
-        
-        this.updateAudioListener();
-
-        if( scene.extensions !== undefined 
-            && scene.extensions.KHR_audio !== undefined)
-        { 
-            for(const emitterReference of scene.extensions.KHR_audio.emitters){
-                let emitter = state.gltf.audioEmitters[emitterReference]
-                if(emitter.type !== "global") // only global emitters can be used in a scene
-                {
-                    continue;
-                }
-
-                this.updateAudioEmitter(state, emitter);
-            }
-        }
-
-        for(const node of this.audioEmitterNodes) {
-            
-            const emitterReference = node.extensions.KHR_audio.emitter;
-            let emitter = state.gltf.audioEmitters[emitterReference]
-            if(emitter.type !== "positional") // only positional emitters can be used in a node
-            {
-                continue;
-            }
-
-            const worldTransform = node.worldTransform;
-            
-            mat4.getTranslation(emitter.position, worldTransform);
-            let resultTransform = vec4.create();
-            vec4.transformMat4(resultTransform, vec4.fromValues(0, 0, -1, 0), worldTransform)
-            vec3.normalize(emitter.orientation,vec3.fromValues(resultTransform[0], resultTransform[1], resultTransform[2]));
-            
-            this.updateAudioEmitter(state, emitter);
-        }     
 
     }
 
@@ -961,10 +786,6 @@ class gltfRenderer
     destroy()
     {
         this.shaderCache.destroy();
-        if(audioContext !== undefined)
-        {
-            this.audioContext.close()
-        }
     }
 }
 
