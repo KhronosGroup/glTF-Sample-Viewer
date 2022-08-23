0cdb9ef2342c93a1f88bb86416937a3187a5b773
Moritz Willy Becher <becher@ux3d.io>
2022-08-23 13:32:41 +0200

    add test for subtract

--- packages/behavior/src/nodes/mathNodes.test.ts
+++ packages/behavior/src/nodes/mathNodes.test.ts
@@ -4,3 +4,8 @@ test('Add', () => {
     const output = mathNodes.add({parameters: {"first": 1, "second": 2}, flow: { next: 1}}, {});
     expect(output.result === 3);
 });
+
+test('Subtract', () => {
+    const output = mathNodes.subtract({parameters: {"first": 1, "second": 2}, flow: { next: 1}}, {});
+    expect(output.result === -1);
+});
