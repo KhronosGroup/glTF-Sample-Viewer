33b4ab6adc6dab157e23ba2e1ff54ec0bbcaa455
Moritz Willy Becher <becher@ux3d.io>
2022-09-06 15:21:17 +0200

    get and set values from gltf state

--- packages/behavior/src/nodes/mathNodes.ts
+++ packages/behavior/src/nodes/mathNodes.ts
@@ -1,16 +1,45 @@
 import { NodeInput, NodeContext, NodeOutput } from "./node-types"
 
+type ArrayType = [number] | [number, number] | [number, number, number] | [number, number, number, number];
+type ScalarOrArrayType = number | [number, number] | [number, number, number] | [number, number, number, number];
+
+function scalarToArray(value: ScalarOrArrayType): ArrayType {
+    if (typeof value === "number") {
+        return [value];
+    }
+    return value;
+}
+
+function arrayToScalar(value: ArrayType): ScalarOrArrayType {
+    if (value.length === 1) {
+        return value[0];
+    }
+    return value;
+}
+
 export const mathNodes = {
     add: (input: NodeInput, context: NodeContext): NodeOutput => {
-        const first = input.parameters.first;
-        const second = input.parameters.second;
+        const first = scalarToArray(input.parameters.first);
+        const second = scalarToArray(input.parameters.second);
         const next = input.flow?.next;
-        return {nextFlow: next, result: {result: first + second}};
+
+        const result = first;
+        for(let i = 0; i < first.length; ++i) {
+            result[i] = first[i] + second[i];
+        }
+
+        return {nextFlow: next, result: {result: arrayToScalar(result)}};
     },
     subtract: (input: NodeInput, context: NodeContext): NodeOutput => {
-        const first = input.parameters.first;
-        const second = input.parameters.second;
+        const first = scalarToArray(input.parameters.first);
+        const second = scalarToArray(input.parameters.second);
         const next = input.flow?.next;
-        return {nextFlow: next, result: {result: first - second}};
+
+        const result = first;
+        for(let i = 0; i < first.length; ++i) {
+            result[i] = first[i] - second[i];
+        }
+
+        return {nextFlow: next, result: {result: arrayToScalar(result)}};
     }
 }
--- source/gltf/behavior.js
+++ source/gltf/behavior.js
@@ -1,24 +1,49 @@
 import { GltfObject } from './gltf_object.js';
 import { Behavior, Interpreter } from '@khronosgroup/gltf-behavior';
 import { JsonPointer } from 'json-ptr';
+import { PointerTargetProperty } from './pointer_target_property';
 class gltfBehavior extends GltfObject
 {
     constructor()
     {
         super();
-        this.interpreter = new Interpreter((pointer, value) => {
-            // const animatedProperty = JsonPointer.get(gltf, property);
-            console.log(`Behavior triggered ${value}`);
-        },
-        (pointer) => {
-            console.log(`Behavior get triggered ${pointer}`);
-            return {};
-        });
+        this.errors = [];
     }
 
     initGl(gltf, webGlContext)
     {
         super.initGl(gltf, webGlContext);
+
+        this.interpreter = new Interpreter((pointer, value) => {
+            const targetProperty = JsonPointer.get(gltf, pointer);
+
+            // Check if the property is a valid target for the behavior to avoid messing up the viewer 
+            // state if the extension is used in an invalid way
+            if (targetProperty === undefined || !(targetProperty instanceof PointerTargetProperty)) {
+                if (!this.errors.includes(pointer)) {
+                    console.warn(`Cannot target ${pointer} in behavior`);
+                    this.errors.push(pointer);
+                }
+                return;
+            }
+
+            targetProperty.setValue(value);
+        },
+        (pointer) => {
+            const targetProperty = JsonPointer.get(gltf, pointer);
+
+            // Check if the property is a valid target for the behavior to avoid messing up the viewer 
+            // state if the extension is used in an invalid way
+            if (targetProperty === undefined || !(targetProperty instanceof PointerTargetProperty)) {
+                if (!this.errors.includes(pointer)) {
+                    console.warn(`Cannot target ${pointer} in behavior`);
+                    this.errors.push(pointer);
+                }
+                return undefined;
+            }
+
+            return targetProperty.value();
+        });
     }
 
     fromJson(jsonBehavior)
@@ -29,6 +54,10 @@ class gltfBehavior extends GltfObject
 
     processEvents(events)
     {
+        if (!this.interpreter) {
+            return;
+        }
+
         for (const [eventName, eventData] of Object.entries(events)) {
             this.behavior.runOnEvent(eventName, this.interpreter, eventData);
         }
