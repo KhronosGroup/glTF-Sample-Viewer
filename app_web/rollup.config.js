9ae9102941c64a232891f19e982a35c16767aca3
Jim Eckerlein <eckerlein@ux3d.io>
2022-09-27 19:37:52 +0200

    Fix another import

--- app_web/rollup.config.js
+++ app_web/rollup.config.js
@@ -2,7 +2,7 @@ import glslify from 'rollup-plugin-glslify';
 import resolve from '@rollup/plugin-node-resolve';
 import builtins from 'rollup-plugin-node-builtins';
 import scss from 'rollup-plugin-scss';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 import alias from '@rollup/plugin-alias';
 import replace from '@rollup/plugin-replace';
