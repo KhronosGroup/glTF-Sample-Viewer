f92b3d798882ca23c242333831c3746b26d2a464
Jim Eckerlein <eckerlein@ux3d.io>
2022-09-28 12:28:21 +0200

    Format code

--- app_web/rollup.config.js
+++ app_web/rollup.config.js
@@ -9,55 +9,53 @@ import replace from '@rollup/plugin-replace';
 import json from '@rollup/plugin-json';
 
 export default {
-  input: 'src/main.js',
-  external: [ 'gl-matrix', 'axios', 'jpeg-js', 'fast-png'],
-  output: [
-    {
-      name: 'SampleViewerApp',
-      file: 'dist/GltfSVApp.umd.js',
-      format: 'umd',
-      sourcemap: true,
-      globals: {
-        'gl-matrix': 'glMatrix',
-        'axios': 'axios',
-        'jpeg-js': 'jpeg',
-        'fast-png': 'png'
-      },
-    }
-  ],
-  plugins: [
-    json(),
-    commonjs({
-
-    }),
-    glslify({
-        include: ['../source/Renderer/shaders/*', '../source/shaders/*'],
-        compress: false
-    }),
-    resolve({
-        browser: true,
-        preferBuiltins: true
-    }),
-    builtins(),
-    scss(),
-    copy({
-        targets: [
-            { src: ["index.html"], dest: "dist/"},
-            { src: ["../assets/models/2.0", "!../asset/models/.git"], dest: "dist/assets/models"},
-            { src: ["../assets/environments/*.hdr", "../assets/environments/*.jpg", "!../asset/environments/.git"], dest: "dist/assets/environments"},
-            { src: ["../assets/images"], dest: "dist/assets"},
-            { src: ["../assets/ui"], dest: "dist/assets"},
-            { src: ["../source/libs/*", "!../source/libs/hdrpng.js"], dest: "dist/libs"}
-        ],
-        copyOnce: true,
-        verbose: true
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify( 'production' ),
-      preventAssignment: true,
-    }),
-    alias({
-      'vue': 'vue/dist/vue.esm.js'
-    }),
-  ]
+    input: 'src/main.js',
+    external: ['gl-matrix', 'axios', 'jpeg-js', 'fast-png'],
+    output: [
+        {
+            name: 'SampleViewerApp',
+            file: 'dist/GltfSVApp.umd.js',
+            format: 'umd',
+            sourcemap: true,
+            globals: {
+                'gl-matrix': 'glMatrix',
+                'axios': 'axios',
+                'jpeg-js': 'jpeg',
+                'fast-png': 'png'
+            },
+        }
+    ],
+    plugins: [
+        json(),
+        commonjs(),
+        glslify({
+            include: ['../source/Renderer/shaders/*', '../source/shaders/*'],
+            compress: false
+        }),
+        resolve({
+            browser: true,
+            preferBuiltins: true
+        }),
+        builtins(),
+        scss(),
+        copy({
+            targets: [
+                { src: ["index.html"], dest: "dist/" },
+                { src: ["../assets/models/2.0", "!../asset/models/.git"], dest: "dist/assets/models" },
+                { src: ["../assets/environments/*.hdr", "../assets/environments/*.jpg", "!../asset/environments/.git"], dest: "dist/assets/environments" },
+                { src: ["../assets/images"], dest: "dist/assets" },
+                { src: ["../assets/ui"], dest: "dist/assets" },
+                { src: ["../source/libs/*", "!../source/libs/hdrpng.js"], dest: "dist/libs" }
+            ],
+            copyOnce: true,
+            verbose: true
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+            preventAssignment: true,
+        }),
+        alias({
+            'vue': 'vue/dist/vue.esm.js'
+        }),
+    ]
 };
