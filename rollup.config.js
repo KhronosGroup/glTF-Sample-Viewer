425531f54efbb76fae1731ce4459cd967bf69a67
Jim Eckerlein <eckerlein@ux3d.io>
2022-09-30 13:07:46 +0200

    Revert "Remove resolve in top level"
    This reverts commit 27c6b92e18e69f1e705596414467abd9ff660157.

--- rollup.config.js
+++ rollup.config.js
@@ -20,6 +20,11 @@ export default {
     ],
     plugins: [
         glslify(),
+        resolve({
+            browser: true,
+            preferBuiltins: false,
+            dedupe: ['gl-matrix', 'axios', 'jpeg-js', 'fast-png']
+        }),
         copy({
             targets: [
                 {
