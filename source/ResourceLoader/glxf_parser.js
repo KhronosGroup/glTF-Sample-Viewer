ba5df1eb5acc51264e6251cd8becee625fa44de1
Mathias Kanzler <kanzler@ux3d.io>
2023-05-12 18:21:42 +0200

    glxf parser merges all assets into one gltf

--- source/ResourceLoader/glxf_parser.js
+++ source/ResourceLoader/glxf_parser.js
@@ -1,23 +1,45 @@
 
-class glxfParser
+import { GltfParser } from "./gltf_parser.js";
+import { GltfMerger } from "./gltf_merger.js";
+class GlxfParser
 {
-    static async convertGlxfToGltf(glxfJson, appendix)
+
+    static findFile(filename, files) 
     {
-        const mergedGLTF = {}; // Initialize an empty merged GLTF object
+        for (const file of files) 
+        {
+            if (file.name === filename)
+            {
+            return file;
+            }
+        }
+        
+        return null; // File not found
+    }
+
+
+
+    static async convertGlxfToGltf(glxf, appendix)
+    {
+        let mergedGLTF = {}; // Initialize an empty merged GLTF object
 
         // Iterate over each asset in the GLXF file
         for (const asset of glxf.assets) {
-          const gltfURI = asset.uri;
-          const gltf = loadGLTF(gltfURI); // Load the GLTF file
-      
-          // Merge the current GLTF with the mergedGLTF object
-          mergedGLTF = mergeGLTF(mergedGLTF, gltf);
+            const gltfURI = asset.uri;
+            console.log("loading glxf asset: "+gltfURI)
+            console.log("glxf appendix: ")
+            console.log(appendix)
+            const assetFile = this.findFile(asset.uri, appendix)
+            const gltfPackage = await GltfParser.loadGltf(assetFile, appendix); // -> { json, data, filename }
+            console.log("gltfPackage: ")
+            console.log(gltfPackage)
+            // Merge the current GLTF with the mergedGLTF object
+            mergedGLTF = GltfMerger.merge(mergedGLTF, gltfPackage.json);
         }
       
- 
-        // Return the GLTF JSON
-        return mergedGLTF;
 
+        // Return the GLTF JSON 
+        return { json: mergedGLTF, data:appendix };
     }
 
 }
