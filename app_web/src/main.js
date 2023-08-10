f5cf8131765d71335cf3303a2844cdef04ebbd9b
Jim Eckerlein <jim.eckerlein@icloud.com>
2023-08-10 19:49:29 +0200

    Delete reduntant wasm files

--- app_web/src/main.js
+++ app_web/src/main.js
@@ -7,8 +7,6 @@ import { Observable, Subject, from, merge } from 'rxjs';
 import { mergeMap, filter, map, multicast } from 'rxjs/operators';
 import { gltfModelPathProvider, fillEnvironmentWithPaths } from './model_path_provider.js';
 
-let mikktspace = null;
-
 async function main() {
     const canvas = document.getElementById("canvas");
     const context = canvas.getContext("webgl2", { alpha: false, antialias: true });
@@ -364,4 +362,4 @@ async function main() {
     window.requestAnimationFrame(update);
 }
 
-export { main, mikktspace };
+export { main };
