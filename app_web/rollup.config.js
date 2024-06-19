c38f0752bb5b6b94e02743d4b00019d477d20243
Patrick Härtl <haertl@ux3d.io>
2024-06-19 12:10:18 +0200

    Cleanup dependencies

--- app_web/rollup.config.js
+++ app_web/rollup.config.js
@@ -1,11 +1,9 @@
 import glslify from 'rollup-plugin-glslify';
 import resolve from '@rollup/plugin-node-resolve';
-import builtins from 'rollup-plugin-node-builtins';
 import scss from 'rollup-plugin-scss';
 import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 import replace from '@rollup/plugin-replace';
-import json from '@rollup/plugin-json';
 import {wasm} from "@rollup/plugin-wasm";
 
 export default {
@@ -21,7 +19,6 @@ export default {
     ],
     plugins: [
         wasm(),
-        json(), // To include model json file
         glslify({
             include: ['../source/Renderer/shaders/*', '../source/shaders/*'],
             compress: false
@@ -31,12 +28,10 @@ export default {
             preferBuiltins: true,
             dedupe: ['gl-matrix', 'jpeg-js', 'fast-png']
         }),
-        builtins(), // Needed for loading assets
         scss(), // Version 4 is not working
         copy({
             targets: [
                 { src: ["index.html", "main.js"], dest: "dist/" },
-                { src: ["../assets/models/Models", "!../asset/models/.git"], dest: "dist/assets/models" },
                 { src: ["../assets/environments/*.hdr", "../assets/environments/*.jpg", "!../asset/environments/.git"], dest: "dist/assets/environments" },
                 { src: ["../assets/images"], dest: "dist/assets" },
                 { src: ["../assets/ui"], dest: "dist/assets" },
