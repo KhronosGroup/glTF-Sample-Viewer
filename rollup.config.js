6c6cabe8cddd5124a018a39847b74ff9a28618f9
David Labode <labode@ux3d.io>
2024-07-25 17:49:56 +0200

    WIP fix copy permission issue while building

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,59 @@ import resolve from '@rollup/plugin-node-resolve';
 import scss from 'rollup-plugin-scss';
 import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
+import del from 'rollup-plugin-delete';
 import replace from '@rollup/plugin-replace';
 import {wasm} from "@rollup/plugin-wasm";
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import license from "rollup-plugin-license";
+import path from "path"
+import fs from "fs"
+
+function copyFiles(from, to, overwrite = false) {
+    return {
+        name: 'copy-files',
+        generateBundle() {
+            const log = msg => console.log('\x1b[36m%s\x1b[0m', msg);
+            log(`copy files: ${from} → ${to}`);
+            if (!fs.existsSync(to)) {
+                fs.mkdirSync(to);
+            }
+            fs.readdirSync(from).forEach(file => {
+                const fromFile = `${from}/${file}`;
+                const toFile = `${to}/${file}`;
+                if (fs.existsSync(toFile) && !overwrite)
+                    return;
+                log(`• ${fromFile} → ${toFile}`);
+                fs.copyFileSync(
+                    path.resolve(fromFile),
+                    path.resolve(toFile)
+                );
+            });
+        }
+    };
+}
+
+function copyFile(from, to, file, overwrite = false) {
+    return {
+        name: 'copy-file',
+        generateBundle() {
+            const log = msg => console.log('\x1b[36m%s\x1b[0m', msg);
+            log(`copy file: ${from} → ${to}`);
+            if (!fs.existsSync(to)) {
+                fs.mkdirSync(to);
+            }
+            const fromFile = `${from}/${file}`;
+            const toFile = `${to}/${file}`;
+            if (fs.existsSync(toFile) && !overwrite)
+                return;
+            log(`• ${fromFile} → ${toFile}`);
+            fs.copyFileSync(
+                path.resolve(fromFile),
+                path.resolve(toFile)
+            );
+        }
+    };
+}
 
 export default {
     strictDeprecations: true,
@@ -31,16 +80,24 @@ export default {
             dedupe: ['gl-matrix', 'jpeg-js', 'fast-png']
         }),
         scss(), // Version 4 is not working
-        copy({
-            targets: [
-                { src: ["index.html", "main.js"], dest: "dist/" },
-                { src: ["assets/images"], dest: "dist/assets" },
-                { src: ["assets/ui"], dest: "dist/assets" },
-                { src: ["glTF-Sample-Render/source/libs/*", "!glTF-Sample-Render/source/libs/hdrpng.js"], dest: "dist/libs" }
-            ],
-            copyOnce: true,
-            verbose: true
-        }),
+        // del({ targets: 'dist/*' }),
+        copyFile(".", "./dist", "index.html", true),
+        copyFile(".", "./dist", "main.js", true),
+        copyFiles("./assets/images", "./dist/assets/images", true),
+        copyFiles("./assets/ui", "./dist/assets/ui", true),
+        copyFiles("./glTF-Sample-Render/source/libs", "./dist/libs", true),
+        copyFiles("./glTF-Sample-Render/assets/images", "./dist/assets/images", true),
+        // copy({
+        //     targets: [
+        //         { src: ["index.html", "main.js"], dest: "dist/" },
+        //         { src: ["assets/images"], dest: "dist/assets" },
+        //         { src: ["assets/ui"], dest: "dist/assets" },
+        //         { src: ["glTF-Sample-Render/source/libs/*", "!glTF-Sample-Render/source/libs/hdrpng.js"], dest: "dist/libs" },
+        //         { src: ["glTF-Sample-Render/assets/images/*"], dest: "dist/assets" }
+        //     ],
+        //     copyOnce: true,
+        //     verbose: true
+        // }),
         replace({
             'process.env.NODE_ENV': JSON.stringify('production'), // This resolves an issue with vue
             preventAssignment: true,
