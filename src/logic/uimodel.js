5dae6e89e5e6508405fb77234d4b2ae66aeccc10
Phillip Hohenester <hohenester@ux3d.io>
2025-10-16 15:18:42 +0200

    drag and drop of folders
    Previously you could not drop a folder containing a gltf (and its images, etc.). This now works.

--- src/logic/uimodel.js
+++ src/logic/uimodel.js
@@ -158,6 +158,7 @@ class UIModel {
                     text = text.replaceAll("\n", "");
                     text = text.replaceAll(" ,", ",");
                     this.app.environmentLicense = text;
+                    // eslint-disable-next-line no-unused-vars
                 } catch (error) {
                     this.app.environmentLicense = "N/A";
                 }
@@ -415,6 +416,36 @@ const getInputObservables = (inputElement, app) => {
                     !file[0].endsWith(".glb") && !file[0].endsWith(".gltf")
             )
         })),
+        map(({ mainFile, additionalFiles }) => {
+            if (mainFile[0].endsWith(".gltf")) {
+                // extract folder path from gltf file
+                let folderPath = mainFile[0];
+                // replace all \ by /
+                folderPath = folderPath.replaceAll("\\", "/");
+                // remove filename
+                folderPath = folderPath.substr(0, folderPath.lastIndexOf("/"));
+
+                if (folderPath !== "") {
+                    // remove folder path from additional files
+                    additionalFiles = additionalFiles.map((file) => {
+                        let filePath = file[0].replaceAll("\\", "/");
+                        if (filePath.startsWith(folderPath)) {
+                            return [
+                                filePath.substr(folderPath.length),
+                                file[1]
+                            ];
+                        } else {
+                            return file;
+                        }
+                    });
+                }
+            }
+
+            return {
+                mainFile: mainFile,
+                additionalFiles: additionalFiles
+            };
+        }),
         filter((files) => files.mainFile !== undefined)
     );
 
